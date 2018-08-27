472096d4266d6e77c4894d98173b5f636908267e
OsFlash <felipe_fzd10@hotmail.com>
2018-08-27 11:12:55 -0300

    Add files via upload

---
+++ Projeto DBZ/export/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1535379016,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/goku-sheet0.png",
+		"images/player-sheet0.png",
+		"images/map-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/colision-sheet0.png",
+		"images/hp_red-sheet0.png",
+		"images/hp_black-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/goku_2-sheet0.png",
+		"images/goku_2-sheet1.png",
+		"images/esfera-sheet0.png",
+		"images/esfera-sheet1.png"
+	]
+}
