43d6258497cbd3a46abc07122b5d8810d86a89d8
OsFlash <felipe_fzd10@hotmail.com>
2018-09-08 16:06:17 -0300

    Add files via upload

---
+++ Projeto DBZ/export2/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1536203094,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/personagem-sheet0.png",
+		"images/personagem-sheet1.png",
+		"images/ground-sheet0.png",
+		"images/coin-sheet0.png",
+		"images/coin-sheet1.png",
+		"images/coin-sheet2.png",
+		"images/npc-sheet0.png",
+		"images/finish-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
